481b9c4ddfaf8a6d5f63bc1dc279fb7b7af97bda
n3rdium <n3rdium@gmail.com>
2024-05-18 22:31:47 +0530

    Update code editor

--- src/code-edit.js
+++ src/code-edit.js
@@ -1,6 +1,15 @@
 document.getElementById("editor").addEventListener("keyup", function() {
     var code = document.getElementById("editor").value;
-    document.getElementById("result").contentWindow.document.open();
-    document.getElementById("result").contentWindow.document.write(code);
-    document.getElementById("result").contentWindow.document.close();
+
+    document.getElementById("result").innerHTML = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        ${code}
+    </body>
+    </html>`
   });
